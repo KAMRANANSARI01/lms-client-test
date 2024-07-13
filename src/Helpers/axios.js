c6d08bf0e7b9a17c0f155de3785fcfdf7bdb6b15
Kamran <123491415+KAMRANANSARI01@users.noreply.github.com>
2024-07-13 21:00:55 +0530

    added live backend url

--- src/Helpers/axios.js
+++ src/Helpers/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5050/api/v1";
+// const BASE_URL = "http://localhost:5050/api/v1";
 // const BASE_URL = "https://lms-api-kamran.up.railway.app/api/v1";
-// const BASE_URL = "https://lms-api-u0zi.onrender.com/api/v1";
+const BASE_URL = "https://lms-server-test-5y2n.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
